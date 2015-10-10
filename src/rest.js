1eae4cf1b0f3edba2be49938b2bd571ac316b715
slurmulon <anon@blaqkmail.com>
2015-10-10 10:53:23 -0700

    :art: rest

--- src/rest.js
+++ src/rest.js
@@ -1,29 +1,68 @@
 import * as http from '../dist/index'
+import * as gooey from '../node_modules/gooey/dist/index'
 
-export class Restource extends http.Http {
+export class Rest extends http.Http {
 
   constructor(slug: String, model: Function, collection: Boolean = true) {
     super() // TODO
 
-    this.slug  = slug
-    this.model = model
-    this.collection = false
+    this.slug = slug
+    this.collection = collection
   }
 
-  set one(slug?: String): Restource {
+  one(id: String): RestResource {
+    const resource = this.copy()
 
+    resource._slug = id
+    resource._collection = false
+
+    return resource
   }
 
-  set all(slug?: String): Restource {
+  all(): RestResource {
+    const resource = this.copy()
 
+    resource._slug = id
+    resource._collection = true
+
+    return resource
   }
 
-  set byId(id: String): Restource {
-
+  copy(): RestResource {
+    return new RestResource(this.slug, this.model, this.collection)
   }
 
-  set current(id: String): Restource {
+}
 
+export class RestService extends gooey.Service {
+
+  constructor(name: String, model: Function, parent?: gooey.Service, children?: Array) {
+    super(...arguments)
+
+    this.resource = new Rest(name, model)
+    this.selected = {entity: null}
   }
 
+  // TODO - add subscription
+  by(id: String): Promise {
+    return this.resource.one(id).get()
+  }
+
+  // TODO - add subscription
+  all(): Promise {
+    return this.resource.all().get()
+  }
+
+  // TODO - add subscription
+  current(): Promise {
+    const entityId = this.selected.entity
+
+    return entityId ? this.resource.by(entityId) : Promise.resolve(null)
+  }
+
+  select(id: String): Promise {
+    this.selected.entity = id
+
+    return this.current()
+  }
 }
